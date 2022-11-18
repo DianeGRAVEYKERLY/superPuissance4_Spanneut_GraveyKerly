7cdfcc02aef560604854577efd5eb8b57608d034
dia12 <dia12@LAPTOP-1CA5JJ5U.education.lakanal.epf.fr>
2022-11-18 12:44:23 +0100

    création objet PremierJeton pour tester le lirecouleur

--- src/superpuissance4/Superpuissance4.java
+++ src/superpuissance4/Superpuissance4.java
@@ -14,6 +14,8 @@ public class Superpuissance4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Jeton PremierJeton = new Jeton("rouge");
+        PremierJeton.lireCouleur();
         
         
         // TODO code application logic here

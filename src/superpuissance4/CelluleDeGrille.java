2c1f08cbed35d0a68a86676cefd5df0f9d6d3f67
alexia <alexia@DESKTOP-3LIGDG0.education.lakanal.epf.fr>
2022-11-18 12:46:39 +0100

    début de la classe CelluleDeGrille

--- src/superpuissance4/CelluleDeGrille.java
+++ src/superpuissance4/CelluleDeGrille.java
@@ -9,5 +9,35 @@ package superpuissance4;
  * @author dia12
  */
 public class CelluleDeGrille {
+    private Jeton jetonCourant; //premier attribut 
+
     
+    //constructeur 
+    public CelluleDeGrille(Jeton jeton, boolean TrouNoir, boolean Desintegrateur){ 
+        jetonCourant=jeton;
+        
+        TrouNoir = false;
+        Desintegrateur = false;
+        
+    } 
+    
+    //jetoncourant devient null
+    public void Celluledegrille(){
+        jetonCourant = null;
+    }
+    
+    // methode pour tester si le jeton est présent ou non 
+    public boolean presenceJeton(){
+        if (jetonCourant != null){
+            return true;
+        }
+        
+    }
+    
+    public void lireCouleurDuJeton(){
+        
+    }
+    
+    private boolean avoirTrouNoir;
+    private boolean avoirDesintegrateur;
 }

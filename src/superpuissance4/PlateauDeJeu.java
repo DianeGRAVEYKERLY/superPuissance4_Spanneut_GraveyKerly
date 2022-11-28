ee36f03d67555425058172cc17500fa8c330b232
dia12 <dia12@LAPTOP-1CA5JJ5U.education.lakanal.epf.fr>
2022-11-28 15:14:42 +0100

    création de la grille de jeu

--- src/superpuissance4/PlateauDeJeu.java
+++ src/superpuissance4/PlateauDeJeu.java
@@ -9,5 +9,20 @@ package superpuissance4;
  * @author dia12
  */
 public class PlateauDeJeu {
+    CelluleDeGrille[][] grille = new CelluleDeGrille [6][7];
+    public PlateauDeJeu (){
+        for (int i=0;i<8;i++){
+            for(int j=0; j<7;j++){
+               grille[i][j] = new CelluleDeGrille();
+            }
+        
+            
+        }
+        
+       
+        
+    }
+    }
+            
     
-}
+

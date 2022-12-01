d374a13c8dde8c134a51a38cab6bece0cc2a18df
alexia <alexia@DESKTOP-3LIGDG0.education.lakanal.epf.fr>
2022-12-01 11:17:57 +0100

    méthode grille remplie, presence jeton et lirecouleur;

--- src/superpuissance4/PlateauDeJeu.java
+++ src/superpuissance4/PlateauDeJeu.java
@@ -22,7 +22,65 @@ public class PlateauDeJeu {
        
         
     }
-    }
-            
     
+    public int ajouterJetonDansColonne(Jeton notre_jeton, int colonne){
+        
+    }
+    
+    public boolean grilleRemplie(){
+        int compteur = 0;
+        for (int i=0;i<8;i++){
+            for(int j=0; j<7;j++){
+               if (grille[i][j]!= null){
+                   compteur +=1; //ajoute +1 à chaque fois qu'une case est remplie 
+               }     
+               }
+        }
+        //compteur = 42 si toute la grille est remplie
+        if (compteur == 42){   
+            return true;
+        }else{
+            return false;
+        }
+         
+    }
+    
+    public boolean presenceJeton(int x, int y){
+        if (grille[x][y] != null){ //on regarde si la cellule est vide ou pas 
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public String lireCouleurDuJeton(int x, int y){ //on lit la couleur du jeton présent dans la case s'il y en a un 
+        grille[x][y].lireCouleurDuJeton();
+        return grille[x][y].lireCouleurDuJeton();
+    }
+    
+    public boolean etreGagnantePourCouleur(String couleur){
+        
+        public boolean ligneGagnantePourCouleur(String couleur){
+            
+            
+        }
+        
+        public boolean colonneGagnantePourCouleur(String couleur){
+            
+        }
+        
+        public boolean diagonaleMontanteGagnantePourCouleur(String couleur){
+            
+        }
+        
+        
+        public boolean diagonaleDesencanteGagnantePourCouleur(String couleur){
+            
+        }
+
+        
+        
+    }
+    
+}
 

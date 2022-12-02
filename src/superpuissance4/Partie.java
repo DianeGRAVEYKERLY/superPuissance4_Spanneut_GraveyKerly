3c68b2e8c9e8a8b8b4264bb41680496152840266
dia12 <dia12@LAPTOP-1CA5JJ5U.education.lakanal.epf.fr>
2022-12-02 09:57:42 +0100

    création attributs de la classe partie , et methode creer etaffecterjeton

--- src/superpuissance4/Partie.java
+++ src/superpuissance4/Partie.java
@@ -4,10 +4,59 @@
  */
 package superpuissance4;
 
+import java.util.Random;
+
 /**
  *
  * @author dia12
  */
 public class Partie {
-  
+  private Joueur[] listeJoueurs= new Joueur[2];
+ private Joueur joueurcourant;
+ private PlateauDeJeu  plateau;
+      public void Partie (Joueur joueur1, Joueur joueur2){
+          listeJoueurs[0]=joueur1;
+          joueur2=listeJoueurs[2];
+          PlateauDeJeu plateau = new PlateauDeJeu();
+          
+}
+public void attribuerCouleurAuxJoueurs() {
+    Random n=new Random();
+    int nbr = n.nextInt(2);
+    
+   
+   
+    
+    
+    if (nbr==1){
+        listeJoueurs[0].AffecterCouleur("rouge");
+        listeJoueurs[1].AffecterCouleur("jaune");
+        
+    }
+    else {
+       listeJoueurs[0].AffecterCouleur("jaune");
+       listeJoueurs[1].AffecterCouleur("rouge");
+    }
+}   
+    public void creerEtAffecterJeton(Joueur j){
+        for (int i=1;i<30;i++){
+            Jeton jetoni= new Jeton (j.lireCouleur());
+            j.AjouterJeton(jetoni);
+            
+        }
+        
+        
+    }
+    public void  placerTrousNoirsEtDesintegrateurs() {
+        Random n=new Random();
+    int i = n.nextInt(2);
+     Random m=new Random();
+    int j = m.nextInt(2);
+    }
+    
+    
+    
+    
+    
+}
 }

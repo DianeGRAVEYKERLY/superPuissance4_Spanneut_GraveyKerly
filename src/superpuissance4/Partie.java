ff983ab8b56887ac0bafc2484898ccae1243fdc4
alexia <alexia@DESKTOP-3LIGDG0.education.lakanal.epf.fr>
2022-12-05 11:15:00 +0100

    continuation de la classe partie, à revoir

--- src/superpuissance4/Partie.java
+++ src/superpuissance4/Partie.java
@@ -5,6 +5,7 @@
 package superpuissance4;
 
 import java.util.Random;   
+import java.util.Scanner;
 
 /**
  *
@@ -25,8 +26,6 @@ public void attribuerCouleurAuxJoueurs() {
     Random n=new Random();
     int nbr = n.nextInt(2);
     
-   
-   
     
     
     if (nbr==1){
@@ -39,6 +38,8 @@ public void attribuerCouleurAuxJoueurs() {
        listeJoueurs[1].AffecterCouleur("rouge");
     }
 }   
+
+
     public void creerEtAffecterJeton(Joueur j){
         for (int i=1;i<30;i++){
             Jeton jetoni= new Jeton (j.lireCouleur());
@@ -93,17 +94,64 @@ public void attribuerCouleurAuxJoueurs() {
     
 }
     public void initialiserPartie(){
+        
+                
         attribuerCouleurAuxJoueurs();
         
         creerEtAffecterJeton(listeJoueurs[0]);
-         creerEtAffecterJeton(listeJoueurs[1]);
-         placerTrousNoirsEtDesintegrateurs() ;
+        creerEtAffecterJeton(listeJoueurs[1]);
+        placerTrousNoirsEtDesintegrateurs() ;
         
          
     }
     
       public void lancerPartie(){
-          while ((plateau.etreGagnantePourCouleur("rouge")=false) 
-                  &&(plateau.etreGagnantePourCouleur("jaune")=false )){
-      }      
-}
+          while (plateau.colonneGagnantePourCouleur("rouge")==false && plateau.colonneGagnantePourCouleur("jaune")==false 
+                  || plateau.ligneGagnantePourCouleur("rouge")==false && plateau.ligneGagnantePourCouleur("jaune")==false
+                  || plateau.diagonaleDesencanteGagnantePourCouleur("rouge")==false && plateau.diagonaleDesencanteGagnantePourCouleur("jaune")==false
+                  || plateau.diagonaleMontanteGagnantePourCouleur("rouge")==false && plateau.diagonaleMontanteGagnantePourCouleur("jaune")==false){
+              
+              
+              
+              System.out.println("c'est à"+joueurcourant.Nom+ "de jouer ("+joueurcourant.couleur+")");
+              
+              Scanner sca =new Scanner (System.in);
+              System.out.println("que voulez-vous faire ?");
+              System.out.println("1 - jouer un jeton");
+              System.out.println("2 - récupérer un jeton");
+              System.out.println("3 - désintégrer un jeton");
+              int choix = sca.nextInt();
+              while (choix > 3 || choix < 1){
+                  System.out.println("erreur : veuillez entrer un chiffre entre 1 et 3");
+                  choix = sca.nextInt();
+              }
+              
+              if ( choix ==1 ){ //le joueur choisit de jouer un jeton 
+                  Scanner scan = new Scanner(System.in);
+                  boolean action;
+                  System.out.println("veuillez saisir une colonne");
+                  int colonne = scan.nextInt() - 1;
+                  while (colonne < 0 || colonne > 6){
+                      System.out.println("erreur : veuillez entrer un chiffre valable");
+                      colonne = scan.nextInt() - 1;
+                  }
+                  action = plateau.ajouterJetonDansColonne(joueurcourant, colonne);
+                  while (!action){
+                      System.out.println("la colonne est pleine, choissisez une autre colonne:");
+                      colonne = scan.nextInt() - 1;
+                      
+                  }
+              }
+              
+              Scanner sc = new Scanner(System.in);
+              System.out.println("entrez la colonne que vous voulez jouer");
+              Jeton j;
+              j.affecterJeton(j);
+              int colonne;
+              colonne = sc.nextInt();
+              plateau.ajouterJetonDansColonne(j, colonne);
+          }
+          
+          
+          
+        
